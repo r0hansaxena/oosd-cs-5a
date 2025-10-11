ceb424876ec047fbb14ab32793411f5ae557797e
r0hansaxena <rohansxn8772@gmail.com>
2025-10-11 06:03:09 +0530

    i1

---
+++ i1.cpp
@@ -0,0 +1,24 @@
+//zingle inheritance
+
+#include <iostream>
+using namespace std;
+
+class A {
+public:
+    void showA(){
+         cout << "This is class A\n";
+     }
+};
+
+class B : public A {
+public:
+    void showB(){ 
+        cout << "This is class B\n"; 
+    }
+};
+
+int main() {
+    B obj;
+    obj.showA();  
+    obj.showB();  
+}
